113873aa0c6750506dede8887cb5280290e5d100
Fialka13 <kristina.mireichik@gmail.com>
2025-10-25 01:13:47 +0300

    Add files via upload

---
+++ Test1 laba 8/Test1 laba 8.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Laba 8\Laba 8.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test1laba8
+{
+	TEST_CLASS(Fraction)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
