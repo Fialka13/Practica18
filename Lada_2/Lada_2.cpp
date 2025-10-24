26d45932f0a4afcfa24e9323c5350ec2a4480b4a
Fialka13 <kristina.mireichik@gmail.com>
2025-10-25 01:22:05 +0300

    Add files via upload

---
+++ Lada_2/Lada_2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <stdexcept>
+
+int recursiveSum(int n) {
+    if (n < 0) {
+        throw std::invalid_argument("Invalid input: n must be non-negative");
+    }
+    if (n == 0) {
+        return 0;
+    }
+    return n + recursiveSum(n - 1);
+}
+
+int divide(int a, int b) {
+    bool Error = false;
+
+    if (b == 0) {
+        bool Error = true;
+    }
+
+    else {
+        bool Error = true;
+
+    }
+    return a / b;
+    return Error;
+}
+void heavyComputation() {
+    volatile double result = 0.0;
+    const long long iterations = 100000000;
+    for (long long i = 0; i < iterations; ++i) {
+        result += i * 0.0000001;
+    }
+    std::cout << "Intensive calculations completed." << std::endl;
+
+    std::cout << "Example result:" << result << std::endl;
+}
+int main() {
+
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+
+    int a = 10;
+    int b = 0;
+    int result = divide(a, b);
+    std::cout << "Division result: " << result << std::endl;
+    std::cout << "Division result: " << result << std::endl;
+    std::cout << "Division result: " << result << std::endl;
+
+    std::cout << "\nIntensive calculations begin..." << std::endl;
+    heavyComputation();
+
+    return 0;
+}
